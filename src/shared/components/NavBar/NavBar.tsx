5646ff8311e7a9f0c13e527a9f1826a9a73be1b1
Nikolay <mikalaj.krutak@gmail.com>
2025-07-06 17:17:02 +0200

    mobile adaptation fix

--- src/shared/components/NavBar/NavBar.tsx
+++ src/shared/components/NavBar/NavBar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
 import styles from "./navbar.module.scss";
+import { useClickContext } from "../../contexts";
 
 export const NavBar: React.FC = () => {
-  const typography = useSelector((state: RootState) => state.typography.data.header);
+  const typography = useSelector(
+    (state: RootState) => state.typography.data.header
+  );
   const media = useSelector((state: RootState) => state.media.data.navbar);
+  const { triggerClick } = useClickContext();
 
   if (!typography || !media) {
     return null;
@@ -16,7 +20,7 @@ export const NavBar: React.FC = () => {
       <a href="/" className={styles.nav__title}>
         {typography?.logoName ?? "ALTANSCHOOL WEB COURSES"}
       </a>
-      <a href="/" className={styles.nav__menuLink}>
+      <a onClick={() => triggerClick()} className={styles.nav__menuLink}>
         <img
           src={media?.burgerMenu ?? "./images/burger-menu.png"}
           alt="menu"
@@ -27,7 +31,11 @@ export const NavBar: React.FC = () => {
       <div className={styles.nav__links}>
         {typography?.links?.map((link: string) => {
           return (
-            <a key={link} className={styles.nav__link} href={`#${link.toLowerCase()}`}>
+            <a
+              key={link}
+              className={styles.nav__link}
+              href={`#${link.toLowerCase()}`}
+            >
               {link}
             </a>
           );
